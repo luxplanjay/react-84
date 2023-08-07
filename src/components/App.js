4ba2f55031758553dfd7128104fbd1628512af0c
Alexander Repeta <alexander.repeta@gmail.com>
2023-08-07 18:29:34 +0300

    prepare examples

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,12 @@ import quizItems from '../data.json';
 import { SearchBar } from './SearchBar/SearchBar';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
-import { HiAcademicCap, HiAdjustments, HiArchive } from 'react-icons/hi';
-import { IconButton } from './IconButton/IconButton';
 
 export const App = () => {
   return (
     <Layout>
       <SearchBar />
       <QuizList items={quizItems} />
-
-      <IconButton variant="primary" size="sm">
-        <HiAcademicCap />
-      </IconButton>
-      <IconButton variant="secondary" size="md">
-        <HiArchive />
-      </IconButton>
-      <IconButton variant="secondary" size="lg">
-        <HiAdjustments />
-      </IconButton>
-
       <GlobalStyle />
     </Layout>
   );
