d68fbe3f354d9ccdd3658f4c88a9c6fb25a011aa
Alexander Repeta <alexander.repeta@gmail.com>
2023-08-23 20:39:56 +0300

    add strcit mode

--- src/index.js
+++ src/index.js
@@ -15,10 +15,10 @@ const theme = {
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <App />
-    <GlobalStyle />
-  </ThemeProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
+  </React.StrictMode>
 );
